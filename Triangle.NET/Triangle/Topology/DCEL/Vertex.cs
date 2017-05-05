120f7853d4f4600b88e2f72d4120b0634bbc5c57
brnkhy <baran.kahyaoglu@mapbox.com>
2017-05-05 21:23:29 +0300

    add third dimension to vertex class

--- Triangle.NET/Triangle/Topology/DCEL/Vertex.cs
+++ Triangle.NET/Triangle/Topology/DCEL/Vertex.cs
@@ -1,68 +1,126 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="Vertex.cs">
+// <copyright file="Vertex.cs" company="">
+// Original Triangle code by Jonathan Richard Shewchuk, http://www.cs.cmu.edu/~quake/triangle.html
 // Triangle.NET code by Christian Woltering, http://triangle.codeplex.com/
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace TriangleNet.Topology.DCEL
+namespace TriangleNet.Geometry
 {
-    using System.Collections.Generic;
+    using System;
+    using TriangleNet.Topology;
 
-    public class Vertex : TriangleNet.Geometry.Point
+    /// <summary>
+    /// The vertex data structure.
+    /// </summary>
+    public class Vertex : Point
     {
-        internal HalfEdge leaving;
-
-        /// <summary>
-        /// Gets or sets a half-edge leaving the vertex.
-        /// </summary>
-        public HalfEdge Leaving
-        {
-            get { return leaving; }
-            set { leaving = value; }
-        }
+        // Hash for dictionary. Will be set by mesh instance.
+        internal int hash;
+        public double z;
+#if USE_ATTRIBS
+        internal double[] attributes;
+#endif
+        internal VertexType type;
+        internal Otri tri;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Vertex" /> class.
         /// </summary>
-        /// <param name="x">The x coordinate.</param>
-        /// <param name="y">The y coordinate.</param>
-        public Vertex(double x, double y)
-            : base(x, y)
+        public Vertex()
+            : this(0, 0, 0)
         {
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Vertex" /> class.
         /// </summary>
-        /// <param name="x">The x coordinate.</param>
-        /// <param name="y">The y coordinate.</param>
-        /// <param name="leaving">A half-edge leaving this vertex.</param>
-        public Vertex(double x, double y, HalfEdge leaving)
-            : base(x, y)
+        /// <param name="x">The x coordinate of the vertex.</param>
+        /// <param name="y">The y coordinate of the vertex.</param>
+        public Vertex(double x, double y, double z)
+            : this(x, y, z, 0)
         {
-            this.leaving = leaving;
         }
 
         /// <summary>
-        /// Enumerates all half-edges leaving this vertex.
+        /// Initializes a new instance of the <see cref="Vertex" /> class.
         /// </summary>
-        /// <returns></returns>
-        public IEnumerable<HalfEdge> EnumerateEdges()
+        /// <param name="x">The x coordinate of the vertex.</param>
+        /// <param name="y">The y coordinate of the vertex.</param>
+        /// <param name="mark">The boundary mark.</param>
+        public Vertex(double x, double y, double z, int mark)
+            : base(x, y, mark)
         {
-            var edge = this.Leaving;
-            int first = edge.ID;
+            this.z = z;
+            this.type = VertexType.InputVertex;
+        }
 
-            do
+#if USE_ATTRIBS
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Vertex" /> class.
+        /// </summary>
+        /// <param name="x">The x coordinate of the vertex.</param>
+        /// <param name="y">The y coordinate of the vertex.</param>
+        /// <param name="mark">The boundary mark.</param>
+        /// <param name="attribs">The number of point attributes.</param>
+        public Vertex(double x, double y, int mark, int attribs)
+            : this(x, y, mark)
+        {
+            if (attribs > 0)
             {
-                yield return edge;
+                this.attributes = new double[attribs];
+            }
+        }
+#endif
 
-                edge = edge.Twin.Next;
-            } while (edge.ID != first);
+        #region Public properties
+
+#if USE_ATTRIBS
+        /// <summary>
+        /// Gets the vertex attributes (may be null).
+        /// </summary>
+        public double[] Attributes
+        {
+            get { return this.attributes; }
+        }
+#endif
+
+        /// <summary>
+        /// Gets the vertex type.
+        /// </summary>
+        public VertexType Type
+        {
+            get { return this.type; }
         }
 
-        public override string ToString()
+        /// <summary>
+        /// Gets the specified coordinate of the vertex.
+        /// </summary>
+        /// <param name="i">Coordinate index.</param>
+        /// <returns>X coordinate, if index is 0, Y coordinate, if index is 1.</returns>
+        public double this[int i]
         {
-            return string.Format("V-ID {0}", base.id);
+            get
+            {
+                if (i == 0)
+                {
+                    return x;
+                }
+
+                if (i == 1)
+                {
+                    return y;
+                }
+
+                throw new ArgumentOutOfRangeException("Index must be 0 or 1.");
+            }
+        }
+
+        #endregion
+
+        public override int GetHashCode()
+        {
+            return this.hash;
         }
     }
 }
