70a47095f0a1df675d93e5f0bd55664951a6665f
bergwerkgis <wb@bergwerk-gis.at>
2017-05-23 10:41:58 +0200

    appveyor: be more verbose about which csproj is being built

--- build.bat
+++ build.bat
@@ -6,12 +6,21 @@ echo ~~~~~~~~~~~~~~~~~~~ %~f0 ~~~~~~~~~~~~~~~~~~~
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+ECHO ------------------------------
+ECHO building .Net 3.5
+ECHO ------------------------------
 msbuild Triangle.NET\Triangle.sln /t:Triangle:Rebuild
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+ECHO ------------------------------
+ECHO building NetStandard
+ECHO ------------------------------
 msbuild Triangle.NET\Triangle.sln /t:TriangleNetStandard:Rebuild
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+ECHO ------------------------------
+ECHO building UWP
+ECHO ------------------------------
 msbuild Triangle.NET\Triangle.sln /t:TriangleUWP:Rebuild
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
